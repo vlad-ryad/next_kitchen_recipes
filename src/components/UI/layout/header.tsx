16aa283aa051664e2bc50c587ae9d6afcb3e96a1
vlad-ryad <work.ryadinsky@gmail.com>
2026-01-26 15:04:11 +0300

    Обновление стилей

---
+++ src/components/UI/layout/header.tsx
@@ -0,0 +1,101 @@
+"use client"
+import { layoutConfig } from "@/config/layout.config";
+import { siteConfig } from "@/config/site.config";
+import { Navbar, NavbarBrand, NavbarContent, NavbarItem, Button } from "@heroui/react";
+import { label } from "framer-motion/client";
+import Image from "next/image";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
+import RegistrationModal from "../modals/registration.modal";
+import LoginModal from "../modals/login.modal";
+import { useState } from "react";
+
+export const Logo = () => {
+	return (
+		<Image
+			src="/logo_kitchen.png"
+			alt={siteConfig.title}
+			width={26}
+			height={26}
+			//sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
+			priority
+		/>
+	);
+};
+
+export default function Header() {
+	const pathname = usePathname();
+
+	const [isRegistrationOpen, setIsRegistrationOpen] = useState(false);
+	const [isLoginOpen, setIsLoginOpen] = useState(false);
+
+	const getNavItems = () => {
+		return siteConfig.navItems.map((item) => {
+			const isActive = pathname === item.href
+			return (
+				<NavbarItem key={item.href}>
+					<Link
+						color="foreground"
+						href={item.href}
+						className={`px-3 py-1 
+									${isActive ? "text-blue-500" : "text-foreground"} 
+									hover:text-blue-300 hover:border
+									hover:border-blue-300 hover:rounded-md
+									transition-colors
+									transition-borders
+									duration-200`}
+					>
+						{item.label}
+					</Link>
+				</NavbarItem>
+			);
+		})
+	}
+
+	return (
+		<Navbar style={{ height: layoutConfig.headerHeight }}>
+			<NavbarBrand>
+				<Link href="/" className="flex gap-1 ">
+					<Logo />
+					<p className="font-bold text-inherit">{siteConfig.title}</p>
+				</Link>
+			</NavbarBrand>
+
+			<NavbarContent className="hidden sm:flex gap-4" justify="center">
+				{getNavItems()}
+			</NavbarContent>
+
+			<NavbarContent justify="end">
+				<NavbarItem className="hidden lg:flex">
+					<Button
+						as={Link}
+						color="primary"
+						href="#"
+						variant="flat"
+						onPress={() => setIsLoginOpen(true)}
+					>
+						Логин
+					</Button>
+				</NavbarItem>
+				<NavbarItem>
+					<Button
+						as={Link}
+						color="primary"
+						href="#"
+						variant="flat"
+						onPress={() => setIsRegistrationOpen(true)}
+					>
+						Регистрация
+					</Button>
+				</NavbarItem>
+
+			</NavbarContent>
+
+			<RegistrationModal
+				isOpen={isRegistrationOpen}
+				onClose={() => setIsRegistrationOpen(false)}
+			/>
+			<LoginModal isOpen={isLoginOpen} onClose={() => setIsLoginOpen(false)} />
+		</Navbar>
+	);
+}
