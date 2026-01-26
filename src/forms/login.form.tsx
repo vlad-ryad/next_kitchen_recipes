fcf78c0907f66d330c553469924741484a1be50b
vlad-ryad <work.ryadinsky@gmail.com>
2026-01-26 15:12:58 +0300

    Создание форм для авторизации и регистрации

---
+++ src/forms/login.form.tsx
@@ -0,0 +1,77 @@
+"use client";
+
+//import { signInWithCredentials } from "@/actions/sign-in";
+import { Button, Form, Input } from "@heroui/react";
+import { useState } from "react";
+
+interface IProps {
+	onClose: () => void;
+}
+
+const LoginForm = ({ onClose }: IProps) => {
+	const [formData, setFormData] = useState({
+		email: "",
+		password: "",
+		confirmPassword: ""
+	});
+
+	const handleSubmit = async (e: React.FormEvent) => {
+		e.preventDefault();
+
+		console.log("Form submitted", formData);
+		//await signInWithCredentials(formData.email, formData.password);
+
+		window.location.reload();
+
+		onClose();
+	};
+
+	return (
+		<Form className="w-full" onSubmit={handleSubmit}>
+			<Input
+				aria-label="Email"
+				isRequired
+				name="email"
+				placeholder="Введите email"
+				type="email"
+				value={formData.email}
+				classNames={{
+					inputWrapper: "bg-default-100",
+					input: "text-sm focus:outline-none "
+				}}
+				onChange={(e) => setFormData({ ...formData, email: e.target.value })}
+				validate={(value) => {
+					if (!value) return "Почта обязательна";
+					return null;
+				}}
+			/>
+			<Input
+				isRequired
+				name="password"
+				placeholder="Введите пароль"
+				type="password"
+				value={formData.password}
+				classNames={{
+					inputWrapper: "bg-default-100",
+					input: "text-sm focus:outline-none "
+				}}
+				onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+				validate={(value) => {
+					if (!value) return "Пароль обязателен";
+					return null;
+				}}
+			/>
+
+			<div className="flex w-[100%]  gap-4 items-center pt-8 justify-end">
+				<Button variant="light" onPress={onClose}>
+					Отмена
+				</Button>
+				<Button color="primary" type="submit">
+					Войти
+				</Button>
+			</div>
+		</Form>
+	);
+};
+
+export default LoginForm;
