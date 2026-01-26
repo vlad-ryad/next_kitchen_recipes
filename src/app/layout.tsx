445dfdee23d0cfdf0614114e6146f108f3016822
vlad-ryad <work.ryadinsky@gmail.com>
2026-01-26 15:16:14 +0300

    обновление: рефакторинг структуры и исправление верстки

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-import Header from "@/components/UI/header";
+import Header from "@/components/UI/layout/header";
 import { Providers } from "@/providers/provider";
 import { siteConfig } from "@/config/site.config";
+import { layoutConfig } from "@/config/layout.config";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -32,7 +33,20 @@ export default function RootLayout({
       >
         <Providers>
           <Header />
-          {children}
+          <main
+            style={{
+              height: `calc(100vh - ${layoutConfig.headerHeight} - ${layoutConfig.footerHeight})`
+            }}
+            className="flex flex-col w-full justify-start items-center"
+          >
+            {children}
+          </main>
+          <footer
+            style={{ height: layoutConfig.footerHeight }}
+            className="w-full flex items-center justify-center py-3"
+          >
+            <p>{siteConfig.description}</p>
+          </footer>
         </Providers>
       </body>
     </html>
