5a06f9698394a27c310797ac13e31ebcef2e3efb
vlad-ryad <work.ryadinsky@gmail.com>
2026-01-26 22:35:08 +0300

    feat: добавление server actions для работы с данными

---
+++ src/actions/register.ts
@@ -0,0 +1,43 @@
+"use server";
+
+
+import { IFormData } from "@/types/form-data";
+import prisma from "@/utils/prisma";
+//import { saltAndHashPassword } from "@/utils/password";
+
+
+export async function registerUser(formData: IFormData) {
+	const { email, password, confirmPassword } = formData;
+
+	if (password !== confirmPassword) {
+		return { error: "Пароли не совпадают" };
+	}
+
+	if (password.length < 6) {
+		return { error: "Пароль должен быть не менее 6 символов" };
+	}
+
+	try {
+		const existingUser = await prisma.user.findUnique({
+			where: { email }
+		});
+
+		if (existingUser) {
+			return { error: "Пользователь с таким email уже существует" };
+		}
+
+		//const pwHash = await saltAndHashPassword(password);
+
+		const user = await prisma.user.create({
+			data: {
+				email: email,
+				password: password,
+			}
+		});
+
+		return user;
+	} catch (error) {
+		console.error("Ошибка регистрации:", error);
+		return { error: "Ошибка при регистрации" };
+	}
+}
