d491389d9da5807d855059cca1f05af8037a88eb
vlad-ryad <work.ryadinsky@gmail.com>
2026-01-27 20:38:45 +0300

    feat: добавлены HOC для переиспользуемой логики

---
+++ src/hoc/app-loader.tsx
@@ -0,0 +1,25 @@
+"use client";
+
+import { useAuthStore } from "@/store/auth.store";
+import { useSession } from "next-auth/react";
+import { useEffect } from "react";
+
+
+interface IProps {
+	children: React.ReactNode;
+}
+
+const AppLoader = ({ children }: IProps) => {
+	const { data: session, status } = useSession();
+	const { isAuth, setAuthState } = useAuthStore();
+
+
+	useEffect(() => {
+		setAuthState(status, session);
+	}, [status, session, setAuthState]);
+
+
+	return <>{children}</>;
+};
+
+export default AppLoader;
