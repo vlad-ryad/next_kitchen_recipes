7933bca9e34051e3622aaebb6a7592027387f4f3
vlad-ryad <work.ryadinsky@gmail.com>
2026-01-28 18:15:54 +0300

    Обновлена схема Prisma и утилиты работы с БД

--- src/utils/prisma.ts
+++ src/utils/prisma.ts
@@ -1,14 +1,21 @@
-
-import { PrismaClient } from "@/generated/prisma/client";
+import { PrismaClient } from "@prisma/client";
 import { withAccelerate } from "@prisma/extension-accelerate";
 
-const globalForPrisma = global as unknown as {
-	prisma: PrismaClient;
+const prismaClientSingleton = () => {
+	return new PrismaClient().$extends(withAccelerate());
+};
+
+type PrismaClientExtended = ReturnType<typeof prismaClientSingleton>;
+
+const globalForPrisma = globalThis as unknown as {
+	prisma?: PrismaClientExtended;
 };
 
 const prisma =
-	globalForPrisma.prisma || new PrismaClient().$extends(withAccelerate());
+	globalForPrisma.prisma ?? prismaClientSingleton();
 
-if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
+if (process.env.NODE_ENV !== "production") {
+	globalForPrisma.prisma = prisma;
+}
 
 export default prisma;
