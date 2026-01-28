33ea513e3345a1cf8d7b1a89e1a5d9482f06b7aa
vlad-ryad <work.ryadinsky@gmail.com>
2026-01-28 18:18:02 +0300

    Обновлён app loader приложения

--- src/hoc/app-loader.tsx
+++ src/hoc/app-loader.tsx
@@ -2,6 +2,7 @@
 
 import { useAuthStore } from "@/store/auth.store";
 import { useIngredientStore } from "@/store/ingredient.store";
+import { useRecipeStore } from "@/store/recipe.store";
 import { useSession } from "next-auth/react";
 import { useEffect } from "react";
 
@@ -14,7 +15,7 @@ const AppLoader = ({ children }: IProps) => {
 	const { data: session, status } = useSession();
 	const { loadIngredients } = useIngredientStore();
 	const { isAuth, setAuthState } = useAuthStore();
-
+	const { loadRecipes } = useRecipeStore();
 	// console.log("ingredients", ingredients);
 
 
@@ -23,10 +24,14 @@ const AppLoader = ({ children }: IProps) => {
 	}, [status, session, setAuthState]);
 
 	useEffect(() => {
-		if (isAuth)
+		if (isAuth) {
 			loadIngredients();
+		}
 	}, [isAuth, loadIngredients]);
 
+	useEffect(() => {
+		loadRecipes();
+	}, [loadRecipes]);
 
 	return <>{children}</>;
 };
